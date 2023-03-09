4ca4d5934f4f5557102dc4a8f4520df18265c1b1
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2023-03-10 01:23:24 +0300

    fixes

--- resources/views/home.view.php
+++ resources/views/home.view.php
@@ -1,7 +1,7 @@
 <?php include view_path() . 'layouts/header.view.php'; ?>   
 
 <div class="max-w-7xl mx-auto py-16 px-4 mt-20 sm:px-6 lg:px-8 border border-gray-200">
-    <h1 class="text-5xl font-bold text-gray-700 text-center">Welcome to Pure PHP Authentication System</h1>
+    <h1 class="text-5xl font-bold text-gray-700 text-center">Welcome to <?= str_replace("-", " ", env('APP_NAME')) ?> System</h1>
     <p class="mt-6 text-xl text-gray-600"></p>
 </div>
 
